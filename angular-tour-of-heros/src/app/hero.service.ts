2276839eab41b162412eb1c4b620047e621c6395
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-23 05:32:11 -0500

    setup service

--- angular-tour-of-heros/src/app/hero.service.ts
+++ angular-tour-of-heros/src/app/hero.service.ts
@@ -3,13 +3,15 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
@@ -24,4 +26,11 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
 }
