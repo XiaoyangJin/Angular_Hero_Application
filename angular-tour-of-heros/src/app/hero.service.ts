6bf7fcf7b82999a6e9f91f0df24534f3300e065a
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-11-23 06:30:30 -0500

    save the change

--- angular-tour-of-heros/src/app/hero.service.ts
+++ angular-tour-of-heros/src/app/hero.service.ts
@@ -63,4 +63,15 @@ export class HeroService {
   }
 
   private heroesUrl = 'api/heroes';  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 }
